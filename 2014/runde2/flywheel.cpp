bf650b761a17751a0d5505519f26f5649de2c4b0
Andreas Jonsterhaug <andreas.vj@icloud.com>
2022-11-13 13:13:03 +0100

    flywheel

--- 2014/runde2/flywheel.cpp
+++ 2014/runde2/flywheel.cpp
@@ -2,51 +2,20 @@
 
 int main() {
 
-    int n;
-    std::cin >> n;
+    int n, p_i, prev;
+    std::cin >> n >> prev;
 
     int profit = 0;
 
-    bool hasBought = false;
-
-    int buyPrice, previousPrice;
-
-    for (int i = 0; i < n; i++) {
-        int pi;
-        std::cin >> pi;
-
-        // Calculate when to buy and when to sell
-        if (i > 0) {
-            if (pi > previousPrice && !hasBought) {
-                // Buy
-                buyPrice = previousPrice;
-                hasBought = true;
-            }
-            else if (pi < previousPrice && hasBought) {
-                // Sell
-                profit += previousPrice - buyPrice;
-                hasBought = false;
-            }
+    for (int i = 1; i < n; i++) {
+        std::cin >> p_i;
+        if (p_i > prev) {
+            profit += p_i-prev;
         }
-
-        previousPrice = pi;
-
+        prev = p_i;
     }
 
-    // Sell the remaining power
-    if (hasBought) {
-        // Don't sell if the sell price is negative
-        if (previousPrice < 0) {
-            profit -= buyPrice;
-        }
-        else {
-            profit += previousPrice - buyPrice;
-        }
-    }
-    else if (previousPrice < 0) {
-        // Buy the last power if you have no power left and the price is negative
-        profit -= previousPrice;
-    }
+    if (p_i < 0) profit -= p_i;
 
     std::cout << profit * 1000 << "\n";
 
