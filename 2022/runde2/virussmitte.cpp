43ef4cb04d071bb657632f568cd83dd3b09d05bb
Andreas Jonsterhaug <andreas.vj@icloud.com>
2023-01-03 21:35:52 +0100

    virussmitte

--- 2022/runde2/virussmitte.cpp
+++ 2022/runde2/virussmitte.cpp
@@ -2,89 +2,94 @@
 #include <vector>
 #include <queue>
 
-//----------------------------------------------------------------
-// Not correct output
+
+// 43/100 score
+
 
 int main() {
 
     int N, M, I, S;
     std::cin >> N >> M >> I >> S;
 
-    int lastMeetingDay;
+    struct meeting {
+        int person1;
+        int person2;
+        int day;
+        bool operator()(meeting& a, meeting& b) {
+            return a.day > b.day;
+        }
+    };
+
+    std::priority_queue<meeting, std::vector<meeting>, meeting> meetings;
+
+    for (int i = 0; i < M; i++) {
+        meeting inp;
+        std::cin >> inp.person1 >> inp.person2 >> inp.day;
+        meetings.push(inp);
+    }
+
+
     int maxSickPeople = 0; 
+    int currentSickPeople = 0; 
 
     std::vector<bool> incubating(N, false);
     std::vector<bool> sick(N, false);
     std::vector<bool> immune(N, false);
 
-    std::queue<int> infections;
+    struct event {
+        int person;
+        int day;
+        bool operator()(event& a, event& b) {
+            return a.day > b.day;
+        }
+    };
 
-    std::queue<std::pair<int, int>> incubatingQueue;
-    std::queue<std::pair<int, int>> sickQueue;
+    std::priority_queue<event, std::vector<event>, event> incubatingQueue;
+    std::priority_queue<event, std::vector<event>, event> sickQueue;
 
-    incubatingQueue.push({0, 0});
-    infections.push(I);
+    incubatingQueue.push({0, I});
+    meetings.push({0, 0, 2147483647});
 
-    for (int i = 0; i < M; i++) {
-        int A, B, D;
-        std::cin >> A >> B >> D;
+    while (!meetings.empty() || !incubatingQueue.empty()) {
 
-        // Make people done incubating sick
-        while (D - incubatingQueue.front().second >= I && !incubatingQueue.empty()) {
+        int person1 = meetings.top().person1;
+        int person2 = meetings.top().person2;
+        int day = meetings.top().day;
 
-            incubating[incubatingQueue.front().first] = false;
-            sick[incubatingQueue.front().first] = true;
-            sickQueue.push({incubatingQueue.front().first, incubatingQueue.front().second + I});
+        while (!incubatingQueue.empty() && incubatingQueue.top().day <= day) {
 
-            std::cout << "sick " << incubatingQueue.front().first << std::endl;
+            while (!sickQueue.empty() && sickQueue.top().day <= incubatingQueue.top().day) {
+                
+                immune[sickQueue.top().person] = true;
+                sick[sickQueue.top().person] = false;
 
+                currentSickPeople--;
+                sickQueue.pop();
+            }
+
+            sick[incubatingQueue.top().person] = true;
+            sickQueue.push({incubatingQueue.top().person, incubatingQueue.top().day + S});
+
+            currentSickPeople++;
+            maxSickPeople = std::max(maxSickPeople, currentSickPeople);
+
+            incubating[incubatingQueue.top().person] = false;
             incubatingQueue.pop();
         }
 
-        // Check if people have become healthy
-        while (D - sickQueue.front().second >= S && !sickQueue.empty()) {
-            sick[sickQueue.front().first] = false;
-            immune[sickQueue.front().first] = true;
-            std::cout << "healthy " << sickQueue.front().first << std::endl;
-            sickQueue.pop();
+        if (sick[person1] && !incubating[person2] && !sick[person2] && !immune[person2]) {
+            incubatingQueue.push({person2, day + I});
+            incubating[person2] = true;
+        }
+        else if (sick[person2] && !incubating[person1] && !sick[person1] && !immune[person1]) {
+            incubatingQueue.push({person1, day + I});
+            incubating[person1] = true;
         }
 
-        // Check if someone gets infected from meeting
-        if (!immune[A] && !immune[B]) {
-            if (sick[A] && (!sick[B] || !incubating[B])) {
-                incubatingQueue.push({B, D});
-                infections.push({D + I});
-                std::cout << "infect " << D << std::endl;
-            }
-            else if (sick[B] && (!sick[A] || !incubating[A])) {
-                incubatingQueue.push({A, D});
-                infections.push({D + I});
-                std::cout << "infect " << D << std::endl;
-            }
-        }
-
-        if (i == M-1) {
-            lastMeetingDay = D;
-        }
+        meetings.pop();
     }
 
-    std::vector<int> day(lastMeetingDay + I + S, 0);
-
-    while (!infections.empty()) {
-        for (int i = 0; i < S; i++) {
-            day[infections.front() + i]++;
-        }
-        infections.pop();
-    }
-
-    for (int i = 0; i < lastMeetingDay + I + S; i++) {
-        std::cout << day[i] << " " << i << std::endl;
-        if (day[i] > maxSickPeople) {
-            maxSickPeople = day[i];
-        }
-    }
-
-    std::cout << maxSickPeople << std::endl;
+    std::cout << maxSickPeople;
 
     return 0;
 }
