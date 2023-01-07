00f04e7a93b04a38a605b11fcc3019f6f18cd413
Andreas Jonsterhaug <andreas.vj@icloud.com>
2023-01-07 01:58:56 +0100

    elevraad

--- 2021/runde2/elevraad.cpp
+++ 2021/runde2/elevraad.cpp
@@ -1,94 +1,51 @@
 #include <iostream>
+#include <algorithm>
 #include <vector>
 
 
 int main() {
 
-    int N, K, actuallyYou;
-    std::cin >> N >> K >> actuallyYou;
+    int N, K, youStartVotes;
+    std::cin >> N >> K >> youStartVotes;
 
-    std::vector<int> candidates;
-    int candidate;
-
-    int you = actuallyYou;
+    std::vector<int> candidates(K - 1);
 
     for (int i = 0; i < K-1; i++) {
-        
-        std::cin >> candidate;
-
-        // Sort the candidates in increasing order using binary search
-
-        // The binary search doesn't work on an empty vector
-        if (candidates.size() == 0) {
-            candidates.push_back(candidate);
-        }
-        else {
-
-            int searchBottom = 0;
-            int searchTop = candidates.size()-1;
-
-            while (true) {
-                
-                int middle = (searchBottom + searchTop) / 2;
-
-                if (candidate > candidates[middle]) {
-                    searchBottom = middle;
-                }
-                else if (candidate < candidates[middle]) {
-                    searchTop = middle;
-                }
-                else {
-                    candidates.insert(candidates.begin() + middle, candidate);
-                    break;
-                }
-
-                if (searchTop - searchBottom <= 1) {
-                    if (candidate < candidates[searchBottom]) {
-                        candidates.insert(candidates.begin() + searchBottom, candidate);
-                    }
-                    else if (candidate > candidates[searchTop]) {
-                        candidates.insert(candidates.begin() + searchTop + 1, candidate);
-                    }
-                    else {
-                        candidates.insert(candidates.begin() + searchTop, candidate);
-                    }
-                    break;
-                }
-            }
-        }
+        std::cin >> candidates[i];
     }
 
-    int equalTopVotes = 1;
+    std::sort(candidates.begin(), candidates.end());
+
+    int you = youStartVotes;
+    int equalTopCandidates = 1;
 
     while (candidates.back() > you) {
 
-        if (equalTopVotes == candidates.size()) {
-            int diff = ((candidates.back() - you)/(1+equalTopVotes));
-            you += diff * equalTopVotes;
-            candidates.back() -= diff;
-            you += candidates.back() - you;
-            candidates.back()--;
+        if (equalTopCandidates != K-1) {
+
+            int secondTopCandidate = candidates[K - equalTopCandidates - 2];
+
+            if (candidates.back() == secondTopCandidate) {
+                equalTopCandidates++;
+                continue;
+            }
+
+            else if (you + (candidates.back() - secondTopCandidate) * equalTopCandidates < secondTopCandidate) {
+                int diff = candidates.back() - secondTopCandidate;
+                you += diff * equalTopCandidates;
+                candidates.back() -= diff;
+                equalTopCandidates++;
+                continue;
+            }
+
         }
 
-        else if (candidates.back() == candidates[candidates.size()-equalTopVotes-1]) {
-            equalTopVotes++;
-        }
-        else if (you + (candidates.back() - candidates[candidates.size()-equalTopVotes-1]) * equalTopVotes < candidates[candidates.size()-equalTopVotes-1]) {
-            int diff = (candidates.back() - candidates[candidates.size()-equalTopVotes-1]);
-            you += diff * equalTopVotes;
-            candidates.back() -= diff;
-            equalTopVotes++;
-        }
-        else {
-            int diff = ((candidates.back() - you)/(1+equalTopVotes));
-            you += diff * equalTopVotes;
-            candidates.back() -= diff;
-            you += candidates.back() - you;
-            candidates.back()--;
-        }
+        int diff = (candidates.back() - you) / (equalTopCandidates + 1);
+        candidates.back() -= diff;
+        you = candidates.back();
     }
 
-    std::cout << you - actuallyYou;
+    std::cout << you - youStartVotes;
 
     return 0;
 }
